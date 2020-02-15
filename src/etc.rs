7676d4d440ddd59c00e7a50c5ca7f659bdc7d010
Josh White <jwhite927@gmail.com>
2020-02-15 15:52:32 -0500

    Reorganized code

---
+++ src/etc.rs
@@ -0,0 +1,43 @@
+use std::io;
+
+pub fn prompt_i32() -> i32 {
+    let mut value = String::new();
+
+    loop {
+    match io::stdin().read_line(&mut value) {
+        Ok(_) => {
+            match value.trim().parse::<i32>() {
+                Ok(num) => return num,
+                Err(_) => {
+                    println!("{} is invalid, please enter an integer.", value.trim());
+                    value = String::new();
+                    continue;
+                },
+            }
+        },
+        Err(_) => {
+            println!("{} is invalid, please enter an integer.", value.trim());
+            value = String::new();
+            continue;
+        },
+    }
+    }
+}
+
+//Counts the number of spaces occupied by an integer
+pub fn num_digits(num: i32) -> usize {
+    if num == 0 { 
+        return 1; 
+    }
+    let mut count: usize = 0;
+    let mut num_copy = num;
+    if num < 0 {
+        count = 1;
+        num_copy *= -1;
+    }
+    while num_copy > 0 {
+        num_copy /= 10;
+        count += 1;
+    }
+    count
+}
--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,10 @@
-mod matrix;
+pub mod matrix;
+pub mod etc;
 use matrix::Matrix;
-use std::io;
 
 fn main() {
     let mut my_matrix = Matrix::new(3,3);
     println!("{}", my_matrix);
     my_matrix.prompt_values();
 
-}
-
-pub fn prompt_i32() -> i32 {
-    let mut value = String::new();
-
-    loop {
-    match io::stdin().read_line(&mut value) {
-        Ok(_) => {
-            match value.trim().parse::<i32>() {
-                Ok(num) => return num,
-                Err(_) => {
-                    println!("{} is invalid, please enter an integer.", value.trim());
-                    value = String::new();
-                    continue;
-                },
-            }
-        },
-        Err(_) => {
-            println!("{} is invalid, please enter an integer.", value.trim());
-            value = String::new();
-            continue;
-        },
-    }
-    }
 }
